9f2a9847d2c4c29d39c7fa1fc87d70b80f7208bb
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-12-27 11:30:48 +0200

    Add saving to localeStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,7 @@ import './Phonebook.css'
 class App extends Component {
 
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: ''
   }
 
@@ -48,6 +45,16 @@ class App extends Component {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  componentDidMount() {
+    this.setState({contacts: JSON.parse(localStorage.getItem('contacts'))})
   }
 
 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -26,7 +26,7 @@ class ContactForm extends Component {
     render() {
         return (
             <form className='form' onSubmit={this.formSubmitHandler}>
-                <label for='name'>Name</label>
+                <label htmlFor='name'>Name</label>
                 <input
                     onChange={this.handleNameChange}
                     value={this.state.name}
@@ -36,7 +36,7 @@ class ContactForm extends Component {
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
                 />
-                <label for='number'>Number</label>
+                <label htmlFor='number'>Number</label>
                 <input
                     onChange={this.handleNameChange}
                     value={this.state.number}
